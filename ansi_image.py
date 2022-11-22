55cd9774e20d1ddd9dc9d2a67053e85cd5df360e
timothy-salazar <timothy.salazar@colorado.edu>
2022-11-22 16:15:30 -0700

    ammending image name

--- ansi_image.py
+++ ansi_image.py
@@ -106,7 +106,7 @@ def main():
     """ This is just for demo purposes
     """
     if len(sys.argv) == 1:
-        path = os.path.join(get_project_dir(), 'images', 'dog.jpg')
+        path = os.path.join(get_project_dir(), 'images', 'doggo.jpg')
     else:
         path = sys.argv[1]
     if len(sys.argv) == 3:
